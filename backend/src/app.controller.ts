479a7af7d0e89e5a5aad59e02b78d55e28ea6c74
Jude Fernandes <jude@octalogic.in>
2019-12-22 00:22:34 +0530

    added github service

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,73 @@
-import { Controller, Get } from "@nestjs/common";
-// import { ConfigService } from "./config/config.service";
+import { Controller, Get, Query, HttpException, HttpStatus } from "@nestjs/common";
+import moment from "moment";
+import { ICommitObject } from "./models/ICommitObject";
+import { GithubService } from "./services/github.service";
 
 @Controller()
 export class AppController {
+  constructor(private readonly githubService: GithubService) {}
+
   @Get("/")
-  async fetch(): Promise<object> {
-    return {
-      message: "success",
-    };
+  async fetch(
+    @Query("sha") sha?: string,
+    @Query("since") since?: string,
+    @Query("until") until?: string,
+  ): Promise<object> {
+    try {
+      let sinceISO: string;
+      let untilISO: string;
+      if (since) {
+        const sinceTimestamp = moment(since, moment.ISO_8601, true);
+        if (!sinceTimestamp.isValid()) {
+          throw new HttpException("Invalid since timestamp", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        sinceISO = sinceTimestamp.toISOString();
+      } else {
+        // fetch history for the past 2 days if unspecified
+        sinceISO = moment()
+          .add(-2, "days")
+          .toISOString();
+      }
+
+      if (until) {
+        const untilTimestamp = moment(until, moment.ISO_8601, true);
+        if (!untilTimestamp.isValid()) {
+          throw new HttpException("Invalid until timestamp", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+
+        untilISO = untilTimestamp.toISOString();
+      } else {
+        untilISO = moment().toISOString();
+      }
+
+      const response = await this.githubService.getCommits(sha, sinceISO, untilISO);
+      // filter out unwanted data
+      const parsedResponse = response.map((item: ICommitObject) => {
+        const {
+          commit: {
+            author,
+            committer,
+            message,
+            verification: { reason, verified },
+          },
+          sha: shaResponse,
+        } = item;
+
+        return {
+          sha: shaResponse,
+          commit: {
+            author,
+            committer,
+            message,
+            verification: { reason, verified },
+          },
+        };
+      });
+
+      return parsedResponse;
+    } catch (error) {
+      throw error;
+    }
   }
 }
