2fd9d457d564f781e74a4dd86c8056f615556e20
Jude Fernandes <jude@octalogic.in>
2019-12-23 19:35:13 +0530

    added username and repo name as inputs for service

--- backend/src/services/github.service.ts
+++ backend/src/services/github.service.ts
@@ -3,20 +3,29 @@ import request from "request-promise-native";
 
 @Injectable()
 export class GithubService {
-  private github_commit_url = "https://api.github.com/repos/facebook/react/commits";
+  private github_commit_url = (username: string = "judemanutd", repoName: string = "github-vcs") =>
+    `https://api.github.com/repos/${username}/${repoName}/commits`;
 
   /***
    * fetch commit history from the github api
    *
+   * @param username - username for the repo
+   * @param repoName - repo name
    * @param sha - SHA or branch to start listing commits from. Default: the repository’s default branch
    * @param since - Only commits after this date will be returned. This is a timestamp in ISO 8601 format
    * @param until - Only commits before this date will be returned. This is a timestamp in ISO 8601 format
    */
-  getCommits = async (sha?: string, since?: string, until?: string) => {
+  getCommits = async (
+    username?: string,
+    repoName?: string,
+    sha?: string,
+    since?: string,
+    until?: string,
+  ) => {
     try {
       const options: request.Options = {
         method: "GET",
-        uri: this.github_commit_url,
+        uri: this.github_commit_url(username, repoName),
         json: true,
         headers: {
           "User-Agent": "vcs-github",
